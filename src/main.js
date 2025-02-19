71cf6c5ad698003ea494706de1f4adef5b56b549
Tristan Heinig <tristan@littlepay.com>
2025-02-19 21:36:41 +1100

    Add suburb search

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ const bounds = L.latLngBounds(
 
 const map = L.map("map", {
   center: bounds.getCenter(),
-  zoom: 9,
+  zoom: 10,
   maxZoom: 13,
-  minZoom: 9,
+  minZoom: 10,
   maxBounds: bounds, // Restrict the map's view to these bounds
   maxBoundsViscosity: 1.0, // Ensure the user cannot drag outside the bounds
   dragging: true, // Allow dragging inside the bounds
@@ -25,6 +25,7 @@ const map = L.map("map", {
 // }).addTo(map);
 
 const colors = ["FFB200", "EB5B00", "D91656", "640D5F"];
+// const colors = ["BE3144", "219B9D", "4C1F7A", "FF8000"];
 
 function style(feature) {
   return {
@@ -71,3 +72,46 @@ const geojson = L.geoJson(suburbs, {
   style: style,
   onEachFeature: onEachFeature,
 }).addTo(map);
+
+// Remove any existing markers
+map.eachLayer(function (layer) {
+  if (layer instanceof L.Marker) {
+    map.removeLayer(layer);
+  }
+});
+
+const suburbsDataList = document.getElementById("list_suburbs");
+suburbs.features.forEach((suburb) => {
+  const option = document.createElement("option");
+  option.value = suburb.properties.name;
+  suburbsDataList.appendChild(option);
+});
+
+const suburbInput = document.getElementById("input_suburbs");
+suburbInput.addEventListener("change", (e) => {
+  const suburbName = e.target.value;
+  console.log("suburbName", suburbName);
+
+  // Reset styles for all features first
+  geojson.eachLayer(function (layer) {
+    geojson.resetStyle(layer);
+  });
+
+  // Loop through each feature to highlight the matching suburb
+  geojson.eachLayer(function (layer) {
+    if (layer.feature.properties.name === suburbName) {
+      layer.setStyle({
+        weight: 3,
+        dashArray: "",
+        fillOpacity: 1,
+        fillColor: "white",
+      });
+
+      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+      }
+
+      map.fitBounds(layer.getBounds());
+    }
+  });
+});
