13420da6e11aaf50f7d8552b4ad70fd1d1c9ee7a
Tristan Heinig <tristan@littlepay.com>
2025-03-19 17:55:02 +1100

    fix correct guess style and delay summary

--- src/game.js
+++ src/game.js
@@ -1,106 +1,106 @@
 window.eventBus = new EventTarget();
 
 function Round(index, suburb) {
-	let guessedSuburb = undefined;
-	let score = 0;
+  let guessedSuburb = undefined;
+  let score = 0;
 
-	return {
-		index,
-		suburb,
-		guess: (guess) => {
-			if (guess === suburb) {
-				score++;
-			}
-			guessedSuburb = guess;
-			return score > 0;
-		},
-		isGuessedCorrectly: () => score > 0,
-		getScore: () => score,
-		getGuessedSuburb: () => guessedSuburb,
-	};
+  return {
+    index,
+    suburb,
+    guess: (guess) => {
+      if (guess === suburb) {
+        score++;
+      }
+      guessedSuburb = guess;
+      return score > 0;
+    },
+    isGuessedCorrectly: () => score > 0,
+    getScore: () => score,
+    getGuessedSuburb: () => guessedSuburb,
+  };
 }
 
 export function Game(suburbs) {
-	let currentRound = undefined;
-	let rounds = [];
+  let currentRound = undefined;
+  let rounds = [];
 
-	function nextRound() {
-		const nextIndex = currentRound.index + 1;
-		if (nextIndex > rounds.length) {
-			currentRound = undefined;
-		} else {
-			currentRound = rounds[nextIndex - 1];
-		}
+  function nextRound() {
+    const nextIndex = currentRound.index + 1;
+    if (nextIndex > rounds.length) {
+      currentRound = undefined;
+    } else {
+      currentRound = rounds[nextIndex - 1];
+    }
 
-		updateState({
-			currentRound,
-			rounds,
-			gameFinished: isGameFinished(),
-		});
-	}
+    updateState({
+      currentRound,
+      rounds,
+      gameFinished: isGameFinished(),
+    });
+  }
 
-	function guessSuburb(guess) {
-		if (isGameFinished()) {
-			console.log("game is finished!");
-			return;
-		}
+  function guessSuburb(guess) {
+    if (isGameFinished()) {
+      console.debug("game is finished!");
+      return;
+    }
 
-		console.log("guessed suburb:", guess);
-		currentRound.guess(guess);
-		if (currentRound.isGuessedCorrectly()) {
-			console.log("correct!");
-		} else {
-			console.log("wrong!");
-		}
-		nextRound();
-	}
+    console.debug("guessed suburb:", guess);
+    currentRound.guess(guess);
+    const correctGuess = currentRound.isGuessedCorrectly();
+    if (correctGuess) {
+      console.debug("correct!");
+    } else {
+      console.debug("wrong!");
+    }
+    nextRound();
+    return correctGuess;
+  }
 
-	function isGameFinished() {
-		return currentRound === undefined;
-	}
+  function isGameFinished() {
+    return currentRound === undefined;
+  }
 
-	function subscribe(observer) {
-		window.eventBus.addEventListener("game:state", (event) => {
-			observer(event.detail);
-		});
-	}
+  function subscribe(observer) {
+    window.eventBus.addEventListener("game:state", (event) => {
+      observer(event.detail);
+    });
+  }
 
-	function updateState(state) {
-		console.log("state updated:");
-		console.table({
-			round: state.currentRound?.index,
-			suburb: state.currentRound?.suburb,
-			guess: state.currentRound?.getGuessedSuburb(),
-			score: state.currentRound?.getScore(),
-		});
+  function updateState(state) {
+    console.debug("state update:");
+    console.debug({
+      round: state.currentRound?.index,
+      suburb: state.currentRound?.suburb,
+    });
 
-		window.eventBus.dispatchEvent(
-			new CustomEvent("game:state", { detail: state }),
-		);
-	}
+    window.eventBus.dispatchEvent(
+      new CustomEvent("game:state", { detail: state })
+    );
+  }
 
-	function start() {
-		rounds = Array.from({ length: 5 }, (_, i) =>
-			Round(i + 1, suburbs[Math.floor(Math.random() * suburbs.length)].name),
-		);
-		currentRound = rounds[0];
+  function start() {
+    rounds = Array.from({ length: 5 }, (_, i) =>
+      Round(i + 1, suburbs[Math.floor(Math.random() * suburbs.length)].name)
+    );
+    currentRound = rounds[0];
 
-		updateState({
-			currentRound,
-			rounds,
-			gameFinished: isGameFinished(),
-		});
-	}
+    updateState({
+      currentRound,
+      rounds,
+      gameFinished: isGameFinished(),
+    });
+  }
 
-	function getCurrentRound() {
-		return currentRound;
-	}
+  function getCurrentRound() {
+    return currentRound;
+  }
 
-	return {
-		guessSuburb,
-		subscribe,
-		start,
-		getCurrentRound,
-		isGameFinished,
-	};
+  return {
+    guessSuburb,
+    subscribe,
+    start,
+    getCurrentRound,
+    isGameFinished,
+  };
 }
--- src/main.js
+++ src/main.js
@@ -7,63 +7,63 @@ import { setupSearch } from "./search";
 import { createMap } from "./map";
 
 const features = suburbsRaw.features.filter(
-	(feature) => feature.properties.name !== undefined,
+  (feature) => feature.properties.name !== undefined
 );
 
 function getCSSVarColor(key) {
-	const rootStyles = getComputedStyle(document.documentElement);
-	return rootStyles.getPropertyValue(key).trim();
+  const rootStyles = getComputedStyle(document.documentElement);
+  return rootStyles.getPropertyValue(key).trim();
 }
 
 const colors = [
-	getCSSVarColor("--color-map-1"),
-	getCSSVarColor("--color-map-2"),
-	getCSSVarColor("--color-map-3"),
-	getCSSVarColor("--color-map-4"),
+  getCSSVarColor("--color-map-1"),
+  getCSSVarColor("--color-map-2"),
+  getCSSVarColor("--color-map-3"),
+  getCSSVarColor("--color-map-4"),
 ];
 
 function showSuburbs() {
-	const { map, geoJson } = createMap(features, {
-		colors,
-		onSelectLayer: (layer) => {
-			if (game.isGameFinished()) {
-				return;
-			}
+  const { map, geoJson } = createMap(features, {
+    colors,
+    onSelectLayer: (layer) => {
+      if (game.isGameFinished()) {
+        return;
+      }
 
-			const suburb = game.getCurrentRound().suburb;
-			const guessedCorrect = game.guessSuburb(layer.feature.properties.name);
+      const suburb = game.getCurrentRound().suburb;
+      const guessedCorrect = game.guessSuburb(layer.feature.properties.name);
 
-			highlightFeature(
-				layer.getElement(),
-				guessedCorrect ? "flicker-correct" : "flicker-wrong",
-			);
+      highlightFeature(
+        layer.getElement(),
+        guessedCorrect ? "flicker-correct" : "flicker-wrong"
+      );
 
-			if (!guessedCorrect) {
-				geoJson.eachLayer((layer) => {
-					if (layer.feature.properties.name === suburb) {
-						highlightFeature(layer.getElement(), "flicker-target");
-					}
-				});
-			}
-		},
-	});
+      if (!guessedCorrect) {
+        geoJson.eachLayer((layer) => {
+          if (layer.feature.properties.name === suburb) {
+            highlightFeature(layer.getElement(), "flicker-target");
+          }
+        });
+      }
+    },
+  });
 
-	function highlightFeature(
-		element,
-		className = "flicker-target",
-		duration = 1000,
-	) {
-		if (element) {
-			element.classList.add(className);
-			setTimeout(() => {
-				element.classList.remove(className);
-			}, duration);
-		}
-	}
+  function highlightFeature(
+    element,
+    className = "flicker-target",
+    duration = 1000
+  ) {
+    if (element) {
+      element.classList.add(className);
+      setTimeout(() => {
+        element.classList.remove(className);
+      }, duration);
+    }
+  }
 }
 
 const suburbs = features.map((feature) => ({
-	name: feature.properties.name,
+  name: feature.properties.name,
 }));
 
 const game = Game(suburbs);
